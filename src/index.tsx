f9e9f7610a71c79d5a2e937279210f63d4cf6062
Max_K <kavokinm@gmail.com>
2021-03-19 13:05:08 +0300

    Changes 19.03.2021 13:05

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "normalize.css";
-import "./styles/index.scss";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'normalize.css';
+import './styles/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+// mkcert -cert-file ./.cert/cert.pem -key-file ./.cert/key.pem localhost
+// HTTPS=true SSL_CRT_FILE=./.cert/cert.pem SSL_KEY_FILE=./.cert/key.pem npm start
--- src/layout/home/components/Dial.tsx
+++ src/layout/home/components/Dial.tsx
@@ -1,15 +1,51 @@
-import React, { useState, useEffect } from "react";
-import ml5 from "ml5";
-import Fr from "./Fr";
-import Notch from "./Notch";
-import Note from "./Note";
+import React, { useState, useEffect } from 'react';
+import ml5 from 'ml5';
+import Fr from './Fr';
+import Notch from './Notch';
+import Note from './Note';
 
 const Dial = (props: any) => {
-  const [deg, setDeg] = useState(0);
+  const [deg, setDeg] = useState([
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+  ]);
 
   useEffect(() => {
-    setup();
+    // setup();
   }, []);
+
+  useEffect(() => {
+    /* console.log(deg); */
+  }, [deg]);
 
   // ==============================
 
@@ -24,36 +60,29 @@ const Dial = (props: any) => {
   }
 
   function startPitch(stream: any, audioContext: any) {
-    pitch = ml5.pitchDetection(
-      "./model",
-      audioContext,
-      stream,
-      modelLoaded
-    ); // modelLoaded
+    pitch = ml5.pitchDetection('./model', audioContext, stream, modelLoaded); // modelLoaded
   }
 
   function modelLoaded() {
-    console.log("Model Loaded");
+    console.log('Model Loaded');
     getPitch();
   }
 
   function getPitch() {
     pitch.getPitch(function (err: Error, pitch: number) {
       if (pitch) {
-        const i = Math.round(
-          Math.log2(Math.round(pitch) / concertPitch) * 12
-        );
+        const i = Math.round(Math.log2(Math.round(pitch) / concertPitch) * 12);
         let steps = Math.abs(i);
         let dir = Math.sign(i) >= 0;
 
         const closest_note = allNotes[(dir ? i : (i % 12) + 12) % 12];
         const closest_pitch = Math.round(concertPitch * 2 ** (i / 12));
 
-        const octave =
-          4 + Math.sign(i) * Math.floor((steps + (dir ? 9 : 2)) / 12);
+        const octave = 4 + Math.sign(i) * Math.floor((steps + (dir ? 9 : 2)) / 12);
 
-        /* console.log("pitch", pitch, "deg", pitch * 1.5); */
-        setDeg(-pitch * 1.5);
+        // console.log('pitch', pitch);
+        /* setDeg(pitch * 1.5); */
+        setDeg((deg) => [...deg.filter((el, i) => i !== 0), pitch]);
       }
       getPitch();
     });
@@ -71,13 +100,13 @@ const Dial = (props: any) => {
     if (isBig) frs.push(<Fr key={i} deg={i} number={frs.length + 1} />);
   }
 
-  let style = { transform: `translate(-50%, -50%) rotate(${deg}deg)` };
-
-  /* console.log(style); */
+  let style = {
+    transform: `translate(-50%, -50%) rotate(${-getMostFrequent(deg) * 1.5}deg)`,
+  };
 
   return (
-    <div className="dial">
-      <div className="dial__inner" style={style}>
+    <div className='dial'>
+      <div className='dial__inner' style={style}>
         {/* NOTCHES */}
         {notches}
         {/* FREQUENCIES */}
@@ -85,12 +114,12 @@ const Dial = (props: any) => {
         {/* NOTES */}
         {/* <Note /> */}
       </div>
-      <div className="dial__housing">
-        <div className="dial__housing-bottom"></div>
+      <div className='dial__housing'>
+        <div className='dial__housing-bottom'></div>
       </div>
-      <div className="dial__center"></div>
-      <div className="dial__hand-cont">
-        <div className="dial__hand"></div>
+      <div className='dial__center'></div>
+      <div className='dial__hand-cont'>
+        <div className='dial__hand'></div>
       </div>
     </div>
   );
@@ -104,21 +133,115 @@ let pitch: any;
 let stream;
 
 const concertPitch = 440;
-const allNotes = [
-  "A",
-  "A#",
-  "B",
-  "C",
-  "C#",
-  "D",
-  "D#",
-  "E",
-  "F",
-  "F#",
-  "G",
-  "G#",
-];
+const allNotes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
 
 let i = Math.round(Math.log2(Math.round(523.25) / concertPitch) * 12);
 let steps = Math.abs(i);
 let dir = Math.sign(i) >= 0;
+
+///
+
+let arr = [
+  73.16887245627998,
+  73.16887245627998,
+  73.16887245627998,
+  73.16887245627998,
+  73.11334451080471,
+  73.11334451080471,
+  73.11334451080471,
+  73.11334451080471,
+  73.11334451080471,
+  73.1379397715822,
+  73.1379397715822,
+  73.1379397715822,
+  73.1379397715822,
+  73.1379397715822,
+  73.15798050785062,
+  73.15798050785062,
+  73.15798050785062,
+  73.15798050785062,
+  73.09373862286938,
+  73.09373862286938,
+  73.09373862286938,
+  73.09373862286938,
+  73.09373862286938,
+  146.28365242718797,
+  146.28365242718797,
+  146.28365242718797,
+  146.28365242718797,
+  73.04628746315598,
+  73.04628746315598,
+  73.04628746315598,
+  73.04628746315598,
+  73.04628746315598,
+  146.6576435303269,
+  146.6576435303269,
+  146.4352376845468,
+  146.4352376845468,
+  146.4352376845468,
+  146.4352376845468,
+  146.4352376845468,
+  145.7967632202069,
+  145.7967632202069,
+  145.7967632202069,
+  145.7967632202069,
+  145.80746537163128,
+  145.80746537163128,
+  145.80746537163128,
+  145.93126358500623,
+];
+
+/* const getMostFrequent = (arr: number[]) => {
+  const sortedArr = arr.sort((a, b) => a - b);
+
+  let maxOccur: number = 0;
+  let maxNumber: number | null = null;
+
+  let occur: number = 0;
+  let rounded: number | null = null;
+
+
+  sortedArr.map((el, i) => {
+    if (rounded !== Math.floor(el)) {
+      rounded = Math.floor(el);
+      occur = 1;
+    } else {
+      ++occur;
+
+      if (occur >= maxOccur) {
+        maxOccur = occur;
+        maxNumber = rounded;
+      }
+    }
+  });
+
+  console.log(maxOccur, maxNumber);
+}; */
+
+const getMostFrequent = (arr: number[]) => {
+  let compare = '';
+  let mostFreq = 0;
+
+  arr.reduce((acc: any, val) => {
+    const floored = Math.floor(val);
+
+    if (floored in acc) {
+      // if key already exists
+      acc[floored]++; // then increment it by 1
+    } else {
+      acc[floored] = 1; // or else create a key with value 1
+    }
+    if (acc[floored] >= compare) {
+      // if value of that key is greater
+      // than the compare value.
+      compare = acc[floored]; // than make it a new compare value.
+      mostFreq = val; // also make that key most frequent.
+    }
+
+    return acc;
+  }, {});
+
+  return mostFreq;
+};
+
+console.log(getMostFrequent(arr));
