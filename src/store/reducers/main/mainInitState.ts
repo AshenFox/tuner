b45fffdde548e270bad46f026f1d3ece2d19a072
Max_K <kavokinm@gmail.com>
2021-06-08 12:17:33 +0300

    Test changes - 12:17

--- src/store/reducers/main/mainInitState.ts
+++ src/store/reducers/main/mainInitState.ts
@@ -3,10 +3,7 @@ import { mainStateInterface } from '../../types/state';
 const mainInitState: mainStateInterface = {
   tunnings: [],
   test: true,
-  fr_arr: [
-    120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120,
-    120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120,
-  ],
+  fr_arr: [120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120],
   most_freq_fr: 120,
 };
 
