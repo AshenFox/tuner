f3111328912d7c2e831927dfcd0fd6889de3a8d7
Max_K <kavokinm@gmail.com>
2021-07-25 12:21:30 +0300

    Adding changes to the layout

--- src/layout/home/index.tsx
+++ src/layout/home/index.tsx
@@ -2,11 +2,49 @@ import React from 'react';
 import Dial from './components/Dial';
 import PitchDetector from './components/PitchDetector';
 import Tuning from './components/Tuning';
+import Select from 'react-select';
+
 interface OwnProps {}
 
 interface StateProps {}
 
 interface DispatchProps {}
+
+const optionsTuningSelect = [
+  { value: 'Guitar standart', label: 'Guitar standart' },
+  { value: 'Drop D', label: 'Drop D' },
+  { value: 'Guitar standart', label: 'Guitar standart' },
+  { value: 'Guitar standart', label: 'Guitar standart' },
+  { value: 'Guitar standart', label: 'Guitar standart' },
+  { value: 'Guitar standart', label: 'Guitar standart' },
+  { value: 'Guitar standart', label: 'Guitar standart' },
+  { value: 'Guitar standart', label: 'Guitar standart' },
+  { value: 'Guitar standart', label: 'Guitar standart' },
+  { value: 'Guitar standart', label: 'Guitar standart' },
+];
+
+const createCustomTheme = (theme: any) => ({
+  ...theme,
+  colors: {
+    ...theme.colors,
+    primary25: '#ec3000',
+    primary: '#ec3000',
+    neutral0: '#533592',
+    neutral80: '#fff',
+  },
+});
+
+const customStyles = {
+  dropdownIndicator: (provided: any) => ({
+    ...provided,
+    paddingLeft: 3,
+    paddingRight: 3,
+  }),
+  menuList: (provided: any) => ({
+    ...provided,
+    maxHeight: 100,
+  }),
+};
 
 type Props = OwnProps & StateProps & DispatchProps;
 
@@ -15,6 +53,16 @@ const Home = (props: Props) => {
     <div className='home'>
       <div className='container'>
         <Dial />
+        <Select
+          className={'tuning-select'}
+          theme={createCustomTheme}
+          options={optionsTuningSelect}
+          isSearchable={false}
+          /* onChange={changeSelectCreated} */
+          /* value={optionsTuningSelect[0]} */
+          instanceId='react-select-created'
+          styles={customStyles}
+        />
         <Tuning />
         <PitchDetector />
       </div>
--- src/layout/tuning-page/index.tsx
+++ src/layout/tuning-page/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Select from 'react-select';
 
 interface OwnProps {}
 
@@ -8,63 +9,183 @@ interface DispatchProps {}
 
 type Props = OwnProps & StateProps & DispatchProps;
 
+const optionsNoteSelect = [
+  { value: 'C', label: 'C' },
+  { value: 'C#', label: 'C#' },
+  { value: 'D', label: 'D' },
+  { value: 'D#', label: 'D#' },
+  { value: 'E', label: 'E' },
+  { value: 'F', label: 'F' },
+  { value: 'F#', label: 'F#' },
+  { value: 'G', label: 'G' },
+  { value: 'G#', label: 'G#' },
+  { value: 'A', label: 'A' },
+  { value: 'A#', label: 'A#' },
+  { value: 'B', label: 'B' },
+];
+
+const optionsOctaveSelect = [
+  { value: '1', label: '1' },
+  { value: '2', label: '2' },
+  { value: '3', label: '3' },
+  { value: '4', label: '4' },
+  { value: '5', label: '5' },
+  { value: '6', label: '6' },
+  { value: '7', label: '7' },
+];
+
+const createCustomTheme = (theme: any) => ({
+  ...theme,
+  colors: {
+    ...theme.colors,
+    primary25: '#ec3000',
+    primary: '#ec3000',
+    neutral0: '#533592',
+    neutral80: '#fff',
+  },
+});
+
+const customStyles = {
+  dropdownIndicator: (provided: any) => ({
+    ...provided,
+    paddingLeft: 3,
+    paddingRight: 3,
+  }),
+  menuList: (provided: any) => ({
+    ...provided,
+    height: 150,
+  }),
+};
+
 const TuningsPage = (props: Props) => {
   return (
     <div className='tuning-page'>
       <div className='container'>
-        <h1 className='tuning-page__header'>Tuning page</h1>
+        <input className='tuning-page__header' value={'Guitar standart'} />
         <div className='tuning-page__list'>
           <div className='tuning-page__string'>
             <span className='tuning-page__number'>1.</span>
             <div className='tuning-page__line'></div>
             <div className='tuning-page__info'>
-              <div className='tuning-page__note-select '>
-                <div className='tuning-page__note tuning-page__note--1'>
-                  <span>C</span>
-                </div>
-                <div className='tuning-page__note tuning-page__note--2'>
-                  <span>C#</span>
-                </div>
-                <div className='tuning-page__note tuning-page__note--3'>
-                  <span>D</span>
-                </div>
-                <div className='tuning-page__note tuning-page__note--4'>
-                  <span>D#</span>
-                </div>
-                <div className='tuning-page__note tuning-page__note--5'>
-                  <span>E</span>
-                </div>
-                <div className='tuning-page__note tuning-page__note--6'>
-                  <span>F</span>
-                </div>
-                <div className='tuning-page__note tuning-page__note--7'>
-                  <span>F#</span>
-                </div>
-                <div className='tuning-page__note tuning-page__note--8'>
-                  <span>G</span>
-                </div>
-                <div className='tuning-page__note tuning-page__note--9'>
-                  <span>G#</span>
-                </div>
-                <div className='tuning-page__note tuning-page__note--10'>
-                  <span>A</span>
-                </div>
-                <div className='tuning-page__note tuning-page__note--11'>
-                  <span>A#</span>
-                </div>
-                <div className='tuning-page__note tuning-page__note--12'>
-                  <span>B</span>
-                </div>
-              </div>
-              {/* <div className='tuning-page__octave-select'>
-                <div className='tuning-page__octave'>1</div>
-                <div className='tuning-page__octave'>2</div>
-                <div className='tuning-page__octave'>3</div>
-                <div className='tuning-page__octave'>4</div>
-                <div className='tuning-page__octave'>5</div>
-                <div className='tuning-page__octave'>6</div>
-                <div className='tuning-page__octave'>7</div>
-              </div> */}
+              <Select
+                className={'tuning-page__note-select'}
+                theme={createCustomTheme}
+                options={optionsNoteSelect}
+                isSearchable={false}
+                /* onChange={changeSelectCreated} */
+                /* value={optionsNoteSelect[0]} */
+                instanceId='react-select-created'
+                styles={customStyles}
+              />
+              <Select
+                className={'tuning-page__octave-select'}
+                theme={createCustomTheme}
+                options={optionsOctaveSelect}
+                isSearchable={false}
+                /* onChange={changeSelectCreated} */
+                /* value={optionsNoteSelect[0]} */
+                instanceId='react-select-created'
+                styles={customStyles}
+              />
+            </div>
+            <div className='tuning-page__line'></div>
+            <div className='tuning-page__controls'>
+              <svg className='tuning-page__delete-icon'>
+                <use href='../svg/sprite.svg#icon__delete'></use>
+              </svg>
+            </div>
+          </div>
+          {/* =================== */}
+          <div className='tuning-page__string'>
+            <span className='tuning-page__number'>2.</span>
+            <div className='tuning-page__line'></div>
+            <div className='tuning-page__info'>
+              <Select
+                className={'tuning-page__note-select'}
+                theme={createCustomTheme}
+                options={optionsNoteSelect}
+                isSearchable={false}
+                /* onChange={changeSelectCreated} */
+                /* value={optionsNoteSelect[0]} */
+                instanceId='react-select-created'
+                styles={customStyles}
+              />
+              <Select
+                className={'tuning-page__octave-select'}
+                theme={createCustomTheme}
+                options={optionsOctaveSelect}
+                isSearchable={false}
+                /* onChange={changeSelectCreated} */
+                /* value={optionsNoteSelect[0]} */
+                instanceId='react-select-created'
+                styles={customStyles}
+              />
+            </div>
+            <div className='tuning-page__line'></div>
+            <div className='tuning-page__controls'>
+              <svg className='tuning-page__delete-icon'>
+                <use href='../svg/sprite.svg#icon__delete'></use>
+              </svg>
+            </div>
+          </div>
+          {/* =================== */}
+          <div className='tuning-page__string'>
+            <span className='tuning-page__number'>3.</span>
+            <div className='tuning-page__line'></div>
+            <div className='tuning-page__info'>
+              <Select
+                className={'tuning-page__note-select'}
+                theme={createCustomTheme}
+                options={optionsNoteSelect}
+                isSearchable={false}
+                /* onChange={changeSelectCreated} */
+                /* value={optionsNoteSelect[0]} */
+                instanceId='react-select-created'
+                styles={customStyles}
+              />
+              <Select
+                className={'tuning-page__octave-select'}
+                theme={createCustomTheme}
+                options={optionsOctaveSelect}
+                isSearchable={false}
+                /* onChange={changeSelectCreated} */
+                /* value={optionsNoteSelect[0]} */
+                instanceId='react-select-created'
+                styles={customStyles}
+              />
+            </div>
+            <div className='tuning-page__line'></div>
+            <div className='tuning-page__controls'>
+              <svg className='tuning-page__delete-icon'>
+                <use href='../svg/sprite.svg#icon__delete'></use>
+              </svg>
+            </div>
+          </div>
+          {/* =================== */}
+          <div className='tuning-page__string'>
+            <span className='tuning-page__number'>4.</span>
+            <div className='tuning-page__line'></div>
+            <div className='tuning-page__info'>
+              <Select
+                className={'tuning-page__note-select'}
+                theme={createCustomTheme}
+                options={optionsNoteSelect}
+                isSearchable={false}
+                /* onChange={changeSelectCreated} */
+                /* value={optionsNoteSelect[0]} */
+                instanceId='react-select-created'
+                styles={customStyles}
+              />
+              <Select
+                className={'tuning-page__octave-select'}
+                theme={createCustomTheme}
+                options={optionsOctaveSelect}
+                isSearchable={false}
+                /* onChange={changeSelectCreated} */
+                /* value={optionsNoteSelect[0]} */
+                instanceId='react-select-created'
+                styles={customStyles}
+              />
             </div>
             <div className='tuning-page__line'></div>
             <div className='tuning-page__controls'>
