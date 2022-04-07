8590d604ce1f91a82bf3d1183f8fb3eac8757b4f
Max_K <kavokinm@gmail.com>
2022-04-07 15:35:53 +0300

    Typescript added to the server.

---
+++ server/index.ts
@@ -0,0 +1,22 @@
+// const express = require('express');
+import express, { Application, Request, Response } from 'express';
+import path from 'path';
+
+const app: Application = express();
+
+// Init Middleware
+const options = { extended: false };
+
+app.use(express.urlencoded(options));
+app.use(express.json());
+
+// Set static folder
+app.use(express.static('build'));
+
+app.get('*', (req: Request, res: Response) => {
+  res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
